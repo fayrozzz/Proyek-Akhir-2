12ead108e32ec1b48273d728749cb0479ea44cf0
Rafael Rizky <firdausifairuz87@gmail.com>
2025-11-11 00:47:48 +0700

    Upgrade list product

--- Product.js
+++ Product.js
@@ -1,38 +1,59 @@
 window.onload = () => {
-  const container = document.querySelector(".hero-box-container");
+  const track = document.querySelector(".product-track");
+  const items = document.querySelectorAll(".product-item");
   const leftBtn = document.querySelector(".arrow.left");
   const rightBtn = document.querySelector(".arrow.right");
 
-  // Gandakan isi kontainer untuk efek looping
-  const original = container.innerHTML;
-  container.innerHTML += original;
+  let index = 0;
+  const allItems = document.querySelectorAll(".product-item");
+itemWidth = allItems[0].offsetWidth + 20;
+  let autoSlide;
 
-  let pos = 0;
-  let speed = 0.3;
-  let singleWidth = 0;
+  // Duplicate item untuk infinite loop
+  items.forEach(item => {
+    const clone = item.cloneNode(true);
+    track.appendChild(clone);
+  });
 
-  // Tunggu konten ter-render
-  setTimeout(() => {
-    singleWidth = container.scrollWidth / 2;
-    loop();
-  }, 300);
+  // Update size saat resize
+  window.addEventListener("resize", () => {
+    itemWidth = items[0].offsetWidth + 20;
+  });
 
-  function loop() {
-    pos -= speed;
-    if (Math.abs(pos) >= singleWidth) pos = 0;
-    container.style.transform = `translateX(${pos}px)`;
-    requestAnimationFrame(loop);
+  // Auto slide
+  function runAuto() {
+    autoSlide = setInterval(() => {
+      index++;
+      track.style.transform = `translateX(${-index * itemWidth}px)`;
+
+      if (index >= items.length) {
+        setTimeout(() => {
+          track.style.transition = "none";
+          index = 0;
+          track.style.transform = `translateX(0)`;
+          setTimeout(() => {
+            track.style.transition = ".4s ease";
+          }, 10);
+        }, 400);
+      }
+    }, 2400);
   }
 
+  runAuto();
+
+  // Arrow manual
   rightBtn.addEventListener("click", () => {
-    pos -= 250;
-    if (Math.abs(pos) >= singleWidth) pos = 0;
-    container.style.transform = `translateX(${pos}px)`;
+    clearInterval(autoSlide);
+    index++;
+    track.style.transform = `translateX(${-index * itemWidth}px)`;
+    runAuto();
   });
 
   leftBtn.addEventListener("click", () => {
-    pos += 250;
-    if (pos > 0) pos = -singleWidth;
-    container.style.transform = `translateX(${pos}px)`;
+    clearInterval(autoSlide);
+    if (index <= 0) index = items.length;
+    index--;
+    track.style.transform = `translateX(${-index * itemWidth}px)`;
+    runAuto();
   });
 };
